8e8febccd60538b4e9e7dd8468a2219ef26991f4
FelipeBarussi <barussifelipe@gmail.com>
2025-06-16 18:40:21 +0200

    Finished Code Data Project

--- Data Project/R part.R
+++ Data Project/R part.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ae64911ff43ef1833ca09a15606c9b7792760de06f42508a8b420dde895da8b
-size 3652
+oid sha256:8795ebedc35e3c820137546196f3518f4e4d563e0aaf3bcef78d24147b6187eb
+size 15726
